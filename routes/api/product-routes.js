632ffd42a906c0513ed174c6f1458870b608cb9b
Tony Phondeth <phondeth@gmail.com>
2021-09-14 14:10:40 -0400

    modified product route with Sequelize query methods

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,34 +1,76 @@
 const router = require('express').Router();
-const {
-    Product,
-    Category,
-    Tag,
-    ProductTag
-} = require('../../models');
+const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
+// Get all products
 router.get('/', (req, res) => {
-    // find all products
-    // be sure to include its associated Category and Tag data
+    // Find all products
+    Product.findAll({
+            attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+            include: [{
+                    model: Category,
+                    attributes: ['id', 'category_name']
+                },
+                {
+                    model: Tag,
+                    attributes: ['id', 'tag_name']
+                }
+            ]
+        })
+        .then(dbProductData => res.json(dbProductData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
-// get one product
+// Get one product
 router.get('/:id', (req, res) => {
-    // find a single product by its `id`
-    // be sure to include its associated Category and Tag data
+    Product.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'product_name',
+            'price',
+            'stock',
+            'category_id'
+        ],
+        include: [{
+                model: Category,
+                attributes: ['id', 'category_name']
+            },
+            {
+                model: Tag,
+                attributes: ['id', 'tag_name']
+            }
+        ]
+    }).then(dbProductData => {
+        if (!dbProductData) {
+            res.status(404).json({
+                message: 'No product found with this id'
+            });
+            return;
+        }
+        res.json(dbProductData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
-// create new product
+// Create new product
 router.post('/', (req, res) => {
     /* req.body should look like this...
-      {
+    {
         product_name: "Basketball",
         price: 200.00,
         stock: 3,
         tagIds: [1, 2, 3, 4]
-      }
+    }
     */
     Product.create(req.body)
         .then((product) => {
@@ -109,7 +151,25 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-    // delete one product by its `id` value
+    // Delete one product by its `id` value
+    Product.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbProductData => {
+        if (!dbProductData) {
+            res.status(404).json({
+                message: 'No product found with this id'
+            });
+            return;
+        }
+        res.json(dbProductData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
