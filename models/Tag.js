2ab4c77be35fcf7d1f1fb59e418c297bcb6a5f64
Tony Phondeth <phondeth@gmail.com>
2021-09-14 21:29:07 -0400

    defined columns for Tag extends Model

--- models/Tag.js
+++ models/Tag.js
@@ -8,7 +8,16 @@ const sequelize = require('../config/connection.js');
 class Tag extends Model {}
 
 Tag.init({
-    // define columns
+    // Define columns
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    tag_name: {
+        type: DataTypes.STRING
+    }
 }, {
     sequelize,
     timestamps: false,
